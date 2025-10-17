a16fd5c979bd96836ce2b5e2a416b1c54672b9d2
a112332 <a112332@alunos.uminho.pt>
2025-10-17 20:46:13 +0100

    feat: [PT] -> [EN]

--- trabalho-pratico/include/utils/validation.h
+++ trabalho-pratico/include/utils/validation.h
@@ -3,14 +3,14 @@
 
 #include <stdbool.h>
 
-bool valid_date(char* date);
-bool valida_code_airport(const char *codigo);
-bool valida_name_city_airport(const char *nome);
-bool valida_country_airport(const char *pais);
-bool valida_latitude_airport(double latitude);
-bool valida_longitude_airport(double longitude);
-bool valida_icao_airport(const char *icao);
-bool valida_type_airport(const char *type);
-void processar_linha_valida(char **campos, int num_campos);
+bool validate_date(char* date);
+bool validate_code_airport(const char *code);
+bool validate_name_city_airport(const char *name);
+bool validate_country_airport(const char *country);
+bool validate_latitude_airport(double latitude);
+bool validate_longitude_airport(double longitude);
+bool validate_icao_airport(const char *icao);
+bool validate_type_airport(const char *type);
+void process_valid_line(char **fields, int num_fields);
 
 #endif
