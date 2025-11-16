e413f52b63adb2fdfd0081c1c54802917d9a565c
Matheus <50931752+matheusm18@users.noreply.github.com>
2025-11-16 18:09:52 +0000

    fix: aircraft_catalog.h encapsulation

--- trabalho-pratico/include/catalog/aircraft_catalog.h
+++ trabalho-pratico/include/catalog/aircraft_catalog.h
@@ -11,9 +11,10 @@ void aircraft_catalog_destroy(AircraftCatalog* manager);
 void aircraft_catalog_add(AircraftCatalog* manager, Aircraft* aircraft);
 Aircraft* get_aircraft_by_identifier(AircraftCatalog* manager, const char* identifier);
 void aircrafts_counter_increment(const char* aircraft_id, AircraftCatalog* manager);
+const Aircraft* aircraft_catalog_iter_next(GHashTableIter* iter);
+void aircraft_catalog_iter_init(const AircraftCatalog* catalog, GHashTableIter* iter);
 
 // getters
 int get_total_aircrafts_in_catalog(AircraftCatalog* catalog);
-GHashTable* aircraft_catalog_get_aircrafts(AircraftCatalog* catalog);
 
 #endif
