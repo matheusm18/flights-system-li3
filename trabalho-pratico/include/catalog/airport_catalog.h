37b8c99eb6968f4f0597854b810c9cc63a2a839c
Matheus <50931752+matheusm18@users.noreply.github.com>
2025-11-16 17:51:26 +0000

    fix: airport_catalog.h encapsulation

--- trabalho-pratico/include/catalog/airport_catalog.h
+++ trabalho-pratico/include/catalog/airport_catalog.h
@@ -10,10 +10,11 @@ AirportCatalog* airport_catalog_create(void);
 void airport_catalog_destroy(AirportCatalog* manager);
 void airport_catalog_add(AirportCatalog* manager, Airport* airport);
 void airport_catalog_sort_all_flights(AirportCatalog* catalog);
+void airport_catalog_iter_init(const AirportCatalog* catalog, GHashTableIter* iter);
+const Airport* airport_catalog_iter_next(GHashTableIter* iter) {
 
 // getters
-GHashTable* airport_catalog_get_airports(AirportCatalog* catalog);
-Airport* get_airport_by_code(AirportCatalog* manager, const char* code);
+const Airport* get_airport_by_code(AirportCatalog* manager, const char* code);
 int airport_catalog_get_count(AirportCatalog* manager);
 
 #endif
