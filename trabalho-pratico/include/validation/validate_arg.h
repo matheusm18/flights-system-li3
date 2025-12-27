ffb09b7d9d9be94dbcb9860ef2d8e3e085fe887b
Débora Caetano <deboraluana0192@gmail.com>
2025-12-27 22:46:13 +0000

    feat: add other queries to validate arguments

--- trabalho-pratico/include/validation/validate_arg.h
+++ trabalho-pratico/include/validation/validate_arg.h
@@ -9,7 +9,11 @@ typedef struct validation_result ValidationResult;
 ValidationResult validate_query1(char **args);
 ValidationResult validate_query2(char **args);
 ValidationResult validate_query3(char **args);
+ValidationResult validate_query4(char **args);
+ValidationResult validate_query5(char **args);
+ValidationResult validate_query6(char **args);
 ValidationResult* validar_query(int query, char **args);
+
 
 const char* validation_result_get_erro(ValidationResult* res);
 bool validation_result_get_ok(ValidationResult* res);
--- trabalho-pratico/src/validation/validate_arg.c
+++ trabalho-pratico/src/validation/validate_arg.c
@@ -31,6 +31,25 @@ ValidationResult validate_query3(char **args) {
     return (ValidationResult){true, NULL};
 }
 
+ValidationResult validate_query4(char **args) {
+    if (!validate_date(args[0]) || !validate_date(args[1]))
+        return (ValidationResult){false, "Formato de data inválido (YYYY-MM-DD)"};
+
+    return (ValidationResult){true, NULL};
+}
+
+ValidationResult validate_query5(char **args) {
+    if (!validate_int_pos(args[0]))
+        return (ValidationResult){false, "Argumento deve ser um inteiro positivo"};
+
+    return (ValidationResult){true, NULL};
+}
+
+ValidationResult validate_query6(char **args) {
+    (void) args;
+    return (ValidationResult){true, NULL};
+}
+
 
 ValidationResult* validar_query(int query, char **args) {
     ValidationResult* res = malloc(sizeof(ValidationResult));
@@ -38,6 +57,9 @@ ValidationResult* validar_query(int query, char **args) {
         case 1: *res = validate_query1(args); break;
         case 2: *res = validate_query2(args); break;
         case 3: *res = validate_query3(args); break;
+        case 4: *res = validate_query4(args); break;
+        case 5: *res = validate_query5(args); break;
+        case 6: *res = validate_query6(args); break;
         default: *res = (ValidationResult){false, "Query desconhecida"}; break;
     }
     return res;
